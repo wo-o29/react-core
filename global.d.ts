2448ea4125038ac28468f80a57e8ade80ceb57ae
wo-o29 <woogur29@gmail.com>
2024-12-16 21:29:48 +0900

    refactor: HTML 타입 설정

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,10 @@
 // 전역 선언 파일 설정
+
+type HTMLTagName = keyof HTMLElementTagNameMap;
 declare namespace JSX {
-  interface IntrinsicElements {
-    // HTML 태그들의 타입 정의
-    [elementName: string]: any;
-  }
+  type IntrinsicElements = {
+    [K in HTMLTagName]: any;
+  };
 }
 
 declare function createElement(
